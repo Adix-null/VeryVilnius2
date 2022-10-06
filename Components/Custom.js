ce0f8e086e2a4ef374d0e639479cd7c9fcdee1dc
Adix-null <adomas@stalas.lt>
2022-10-06 21:33:58 +0300

    Migration
    Migrated to Github

---
+++ Components/Custom.js
@@ -0,0 +1,50 @@
+class PCNavbar extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+    fetch('/Components/PC-nav.html').then(response => response.text()).then(text => this.innerHTML = text);
+    }
+}
+customElements.define("navbar-component-pc", PCNavbar);
+
+class PhoneNavbar extends HTMLElement{
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+    fetch('/Components/phone-nav.html').then(response => response.text()).then(text => this.innerHTML = text);
+    }
+}
+customElements.define("navbar-component-phone", PhoneNavbar);
+
+class Language extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        //Not a perfect solution
+        var path = this.getAttribute("src").toString().slice(0, -7); // removes en.html from string
+        this.innerHTML = `
+        <div class="language-container">
+            <a href="${path.concat("en.html")}"> EN | </a>
+            <a href="${path.concat("ru.html")}"> RU |</a>
+            <a href="${path.concat("lt.html")}"> LT </a>
+        </div>
+        `
+    }
+}
+customElements.define("language-select", Language);
+
+
+function ToggleOptions(Name, element){
+    var doc = document.getElementById(Name);
+    const upArr = '\u02C4';//^
+    const dwArr = '\u02C5';//v
+    //toggle arrow
+    element.innerHTML = element.innerHTML.slice(0,-1).concat(element.innerHTML.slice(-1)===upArr ? dwArr : upArr);
+    //toggle display
+    doc.style.display = (doc.style.display=="none"|| doc.style.display=="") ? "block" : "none";
+}
---
+++ Contact-us.php
@@ -0,0 +1,58 @@
+<?php
+
+use PHPMailer\PHPmailer\PHPMailer;
+use PHPMailer\PHPmailer\Exception;
+require 'sendemail/src/Exception.php';
+require 'sendemail/src/PHPMailer.php';
+require 'sendemail/src/SMTP.php';
+
+//REQUIRES CHANGING
+$to = 'adixnull0@gmail.com';
+
+if(isset($_POST['submit']))
+{
+    $mail = new PHPMailer(true);
+
+    $mail->IsSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->Username = $to;
+    //Should not be a security vulnerability
+    $mail->Password = 'ctkspxqtnlacbkvk';
+    $mail->SetFrom($_POST['adress'],  $_POST['name'] . ', Very Vilnius Website'); //sender
+    $mail->AddAddress($to); //recipient
+
+    $mail->isHTML(false);
+    $mail->Subject = $_POST['subject'];
+    $mail->Body = $_POST['message'];
+    
+    try
+    {
+        $mail->Send();
+        echo "
+        <script> 
+            alert('Email sent sucessfully'); 
+        </script>";
+    }
+    catch(Exception $e)
+    {
+        $strmg = 'Error: ' . $e->getMessage();
+        echo  "
+        <script> 
+            alert($strmg); 
+        </script>";;
+    }
+    echo "
+    <script> 
+        document.location.href = 'Extra-Info.html';
+    </script>";
+}
+else
+{
+    echo "submit button is not set";
+}
+?>
