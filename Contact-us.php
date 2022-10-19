4a7ec38a77a1157e3842c16ca9ef4d0ccb91e8c1
Adix-null <adomas@stalas.lt>
2022-10-19 22:02:27 +0300

    Routed php email to proper address

--- Contact-us.php
+++ Contact-us.php
@@ -2,44 +2,41 @@
 
 use \PHPMailer\PHPmailer\PHPMailer;
 use \PHPMailer\PHPmailer\Exception;
+
 require 'sendemail/src/Exception.php';
 require 'sendemail/src/PHPMailer.php';
 require 'sendemail/src/SMTP.php';
 
 //REQUIRES CHANGING
-$to = 'info@veryvilnius.com';
+$to = 'veryvilnius@gmail.com';
 
-if(isset($_POST['submit']))
-{
+if (isset($_POST['submit'])) {
     $mail = new PHPMailer(true);
 
     $mail->IsSMTP();
-    $mail->Host = 'smtp-mail.outlook.com';
-    
+    $mail->Host = 'smtp.gmail.com';
+
     $mail->SMTPAuth = true;
     $mail->SMTPSecure = 'ssl';
-    $mail->Port = 587;
+    $mail->Port = 465;
 
     $mail->Username = $to;
     //Should not be a security vulnerability
-    $mail->Password = 'ctkspxqtnlacbkvk';
+    $mail->Password = 'kjpwemfehjxbxtpb';
     $mail->SetFrom($_POST['adress'],  $_POST['name'] . ', Very Vilnius Website'); //sender
     $mail->AddAddress($to); //recipient
 
     $mail->isHTML(false);
     $mail->Subject = $_POST['subject'];
     $mail->Body = $_POST['message'];
-    
-    try
-    {
+
+    try {
         $mail->Send();
         echo "
         <script> 
             alert('Email sent sucessfully'); 
         </script>";
-    }
-    catch(Exception $e)
-    {
+    } catch (Exception $e) {
         $strmg = 'Error: ' . $e->getMessage();
         echo  "
         <script> 
@@ -50,8 +47,6 @@ if(isset($_POST['submit']))
     <script> 
         document.location.href = 'info.html';
     </script>";
-}
-else
-{
+} else {
     echo "submit button is not set";
 }
