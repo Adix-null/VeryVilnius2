37a1a20de670d6683aa21c2d5a40c1c4701c7094
Adix-null <adomas@stalas.lt>
2022-12-06 20:17:51 +0200

    Small upgrades
    Added Vilnius directions and changed photos

--- Components/Custom.js
+++ Components/Custom.js
@@ -1,40 +1,44 @@
-class PCNavbar extends HTMLElement{
-    constructor(){
-        super();
-    }
+class PCNavbar extends HTMLElement {
+	constructor() {
+		super();
+	}
 
-    connectedCallback(){
-    fetch('/Components/pc-nav.html').then(response => response.text()).then(text => this.innerHTML = text);
-    }
+	connectedCallback() {
+		fetch("/Components/pc-nav.html")
+			.then((response) => response.text())
+			.then((text) => (this.innerHTML = text));
+	}
 }
 customElements.define("navbar-component-pc", PCNavbar);
 
-class PhoneNavbar extends HTMLElement{
-    constructor(){
-        super();
-    }
-    connectedCallback(){
-    fetch('/Components/phone-nav.html').then(response => response.text()).then(text => this.innerHTML = text);
-    }
+class PhoneNavbar extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		fetch("/Components/phone-nav.html")
+			.then((response) => response.text())
+			.then((text) => (this.innerHTML = text));
+	}
 }
 customElements.define("navbar-component-phone", PhoneNavbar);
 
-class Language extends HTMLElement{
-    constructor(){
-        super();
-    }
+class Language extends HTMLElement {
+	constructor() {
+		super();
+	}
 
-    connectedCallback(){
-        //Not a perfect solution
-        var path = this.getAttribute("src").toString().slice(0, -7); // removes en.html from string
-        this.innerHTML = `
+	connectedCallback() {
+		//Not a perfect solution
+		var path = this.getAttribute("src").toString().slice(0, -7); // removes en.html from string
+		this.innerHTML = `
         <div class="language-container">            
             <a href="${path.concat("en.html")}"> EN | </a>
             <a href="${path.concat("ru.html")}"> RU |</a>
             <a href="${path.concat("lt.html")}"> LT </a>
         </div>
         `;
-    }
+	}
 }
 customElements.define("language-select", Language);
 
@@ -61,32 +65,30 @@ customElements.define("language-select", Language);
 // }
 // customElements.define("photosphere-script", Photosphere);
 
-
-function ToggleOptions(Name, element){
-    var doc = document.getElementById(Name);
-    const upArr = '\u02C4';//^
-    const dwArr = '\u02C5';//v
-    //toggle arrow
-    element.innerHTML = element.innerHTML.slice(0,-1).concat(element.innerHTML.slice(-1) === upArr ? dwArr : upArr);
-    //toggle display
-    doc.style.display = (doc.style.display=="none"|| doc.style.display=="") ? "block" : "none";
+function ToggleOptions(Name, element) {
+	var doc = document.getElementById(Name);
+	const upArr = "\u02C4"; //^
+	const dwArr = "\u02C5"; //v
+	//toggle arrow
+	element.innerHTML = element.innerHTML
+		.slice(0, -1)
+		.concat(element.innerHTML.slice(-1) === upArr ? dwArr : upArr);
+	//toggle display
+	doc.style.display =
+		doc.style.display == "none" || doc.style.display == "" ? "block" : "none";
 }
 
-function ToggleNav(elm)
-{
-    const rgArr = '\u02C3';//>
-    const lfArr = '\u02C2';//<
-    if(elm.textContent === lfArr)
-    {
-        elm.innerHTML = rgArr;
-        elm.style.left = "50px";
-        document.querySelector(".navbar.phone").style.display = "none";
-    }
-    else
-    {
-        elm.innerHTML = lfArr;
-        elm.style.left = "calc(60vw + 20px)";
-        document.querySelector(".navbar.phone").style.display = "block";
-        document.querySelector(".navbar.phone").style.minWidth = "60vw";
-    }
+function ToggleNav(elm) {
+	const rgArr = "\u02C3"; //>
+	const lfArr = "\u02C2"; //<
+	if (elm.textContent === lfArr) {
+		elm.innerHTML = rgArr;
+		elm.style.left = "50px";
+		document.querySelector(".navbar.phone").style.display = "none";
+	} else {
+		elm.innerHTML = lfArr;
+		elm.style.left = "calc(60vw + 20px)";
+		document.querySelector(".navbar.phone").style.display = "block";
+		document.querySelector(".navbar.phone").style.minWidth = "60vw";
+	}
 }
